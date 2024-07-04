53744c6157ba8dbb343cb30271dd48f16101c95b
WonyeolHEO <hyy96hyy@naver.com>
2024-07-04 19:55:05 +0900

    Mapkit App initial Setting

---
+++ SwiftUIMapKit/SwiftUIMapKitApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIMapKitApp.swift
+//  SwiftUIMapKit
+//
+//  Created by wonyoul heo on 7/4/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIMapKitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
